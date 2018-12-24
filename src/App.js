6b8bb8ed62cbd8fe279d79c3c622fe8a59920e9b
paultrh <paultr_h@epita.fr>
2018-12-24 12:14:19 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ class App extends Component {
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.google.fr"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn Google
           </a>
         </header>
       </div>
